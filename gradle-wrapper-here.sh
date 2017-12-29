1faa3a48e4139a1d689ad07e5a40b230cee46726
Ray Holder <ray.holder+github@gmail.com>
2017-12-28 20:06:31 -0600

    Update to Gradle 4.4.1 and prepare for 0.5.0 release

--- gradle-wrapper-here.sh
+++ gradle-wrapper-here.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2014-2016 Ray Holder
+# Copyright 2014-2017 Ray Holder
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,8 +22,8 @@ __DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
 __BASE__="$(basename "${0}")"
 __FILE__="${__DIR__}/${__BASE__}"
 
-VERSION=0.4.0
-GRADLE_VERSION=3.2.1
+VERSION=0.5.0
+GRADLE_VERSION=4.4.1
 
 function usage () {
         cat << EOF
